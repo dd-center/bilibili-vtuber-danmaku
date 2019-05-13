54f3e651766091ff11b78277c1c637ecd3d50b0a
simon3000 <simon3000@163.com>
2019-05-13 16:03:00 +0800

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,26 +1,26 @@
-const fs = require('fs').promises
+// const fs = require('fs').promises
 
-const nodejieba = require('nodejieba')
+// const nodejieba = require('nodejieba')
 
-// const Server = require('socket.io')
-// const io = new Server(9002, { serveClient: false, path: '/' })
+const Server = require('socket.io')
+const io = new Server(9002, { serveClient: false, path: '/' })
 
-nodejieba.load({
-  userDict: 'dictionary/userdict.txt',
-})
+// nodejieba.load({
+//   userDict: 'dictionary/userdict.txt',
+// })
 
 // io.on('connection', socket => {
 //   socket.on('wow', console.log)
 // })
-;
-(async () => {
-  let day = String(await fs.readFile('12235923/2019-5-11.txt'))
-    .split('\n')
-    .filter(w => !(w.includes('TIME') && w.includes('ONLINE') || w.includes('SPEAKERNUM')))
-    .join('\n')
-  nodejieba.extract(day, 250).forEach(w => console.log(`${w.word};${Math.round(w.weight)}`))
-  // nodejieba.extract(day, 100).forEach(w => console.log(w))
-})()
+// ;
+// (async () => {
+//   let day = String(await fs.readFile('12235923/2019-5-11.txt'))
+//     .split('\n')
+//     .filter(w => !(w.includes('TIME') && w.includes('ONLINE') || w.includes('SPEAKERNUM')))
+//     .join('\n')
+//   nodejieba.extract(day, 250).forEach(w => console.log(`${w.word};${Math.round(w.weight)}`))
+//   // nodejieba.extract(day, 100).forEach(w => console.log(w))
+// })()
 
 
 // (async () => {
