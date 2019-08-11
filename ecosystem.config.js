be9aba7e3f97577f7e2910b43be54f18c5e56da1
simon3000 <simon3000@163.com>
2019-08-12 00:22:56 +0200

    remove monster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,5 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-  }, {
-    name: 'vdMonster',
-    script: 'monster.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
   }],
 }
